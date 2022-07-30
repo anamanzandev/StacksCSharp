bc1bbf35a2b1d085491eedb5d2cb2697eccc17be
Ana Manzan <ana.manzan@gmail.com>
2022-07-30 16:43:01 -0300

    Fixed warning at Node.cs

--- StacksCSharp/Node.cs
+++ StacksCSharp/Node.cs
@@ -3,7 +3,7 @@ namespace StacksCSharp
     public class Node 
     {
         public int Element { get; set; }
-        public Node Next { get; set; }
+        public Node? Next { get; set; }
 
         public Node(int element)
         {
